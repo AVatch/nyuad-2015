07496e766e4dad54b4ee224d7d242d798d1d9dc6
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-04-09 01:36:02 -0400

    fab triggers modal

--- linkbump/www/js/articles/controllers.articles.js
+++ linkbump/www/js/articles/controllers.articles.js
@@ -1,8 +1,9 @@
 angular.module('nyuad2015.controllers.articles', [])
 
-.controller('ArticleListController', ['$scope', 'Article',
-  function($scope, Article){
+.controller('ArticleListController', ['$scope', '$ionicModal', 'Article',
+  function($scope, $ionicModal, Article){
 
+    // load the articles
     $scope.articleList = [];
     Article.getList().then(
       function(s){
@@ -10,4 +11,31 @@ angular.module('nyuad2015.controllers.articles', [])
           $scope.articleList = s.data.results;
         }
       },function(e){console.log(e);});
+
+
+    // set up the modal
+    $ionicModal.fromTemplateUrl('js/articles/views/add_article_modal.tmpl.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
 }]);
