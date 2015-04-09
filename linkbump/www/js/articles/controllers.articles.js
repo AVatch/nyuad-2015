7f231ee4a39c09f3103458a8e75a494806f0d57a
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-04-09 01:27:51 -0400

    list is populated from server articles

--- linkbump/www/js/articles/controllers.articles.js
+++ linkbump/www/js/articles/controllers.articles.js
@@ -3,5 +3,11 @@ angular.module('nyuad2015.controllers.articles', [])
 .controller('ArticleListController', ['$scope', 'Article',
   function($scope, Article){
 
-    Article.getList().then(function(s){console.log(s);}, function(e){console.log(e);});
+    $scope.articleList = [];
+    Article.getList().then(
+      function(s){
+        if(s.status==200){
+          $scope.articleList = s.data.results;
+        }
+      },function(e){console.log(e);});
 }]);
